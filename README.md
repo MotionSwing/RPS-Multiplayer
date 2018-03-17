b16ba7112261fdf8b828418080ae598043aa64ca
MotionSwing <jon.white2@gmail.com>
2018-03-17 04:56:32 -0400

    v1.0.3 commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # RPS-Multiplayer
+
+## Purpose
+
+Challenge a friend to a quick game of rock, paper, scissors. Let your oppontent know just how badly they will lose to your awe inspiring skill. Your methods of attack include: rocks, paper and scissors.
+
+Points to Remember:
+* Rock beats Scissors
+* Paper beats Rock
+* Scissors beats Paper
+
+[Click here to play!](https://motionswing.github.io/Gif-Generator/)
+
+## Author
+
+This project is maintained and written by 
+[Jon White](https://motionswing.github.io/)
--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,5 +1,5 @@
-displayPlayerTurn("hide");
-updateTurnStatus("hide","","","");
+game.displayPlayerTurn("hide");
+game.updateTurnStatus("hide","","","");
 
 // ==============================
 // ======= Initialize App =======
@@ -15,7 +15,6 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-
 
 // ==============================
 // ===== Detect Connections =====
@@ -70,15 +69,11 @@ $("#chatSendBtn").on('click', function(event) {
 				comment: chatComment,
 				commentTime: firebase.database.ServerValue.TIMESTAMP
 			});
-
-			// const chat = $("#chatHistory");
-			// chat.scrollTop(chat.prop("scrollHeight"));
 		}		
 	}
 });
 
 database.ref("chat").orderByChild("commentDate").limitToLast(10).on('child_added', function(childSnapshot) {
-	console.log('chat add comment');
 	const comment = $("<div class='comment player-"+ childSnapshot.val().playerNum +"'>");
 	const commentText = $("<p class='commentText'>")
 		.html("<span class='playerName'>"+childSnapshot.val().playerName + ":</span> " + childSnapshot.val().comment);
@@ -89,20 +84,23 @@ database.ref("chat").orderByChild("commentDate").limitToLast(10).on('child_added
 	comment.append(commentText,commentTime);
 
 	$("#chatHistory").append(comment);
+
+	// Play sound
+	if(game.hasStarted && childSnapshot.val().playerNum === 1){
+		game.p1_comment_sound.play();
+	}else if(game.hasStarted && childSnapshot.val().playerNum === 2){
+		game.p2_comment_sound.play();
+	}
+
 	const chat = $("#chatHistory");
-	chat.scrollTop(chat.prop("scrollHeight"));
+	// chat.scrollTop(chat.prop("scrollHeight"));
+	chat.animate({ scrollTop: chat.prop("scrollHeight")}, 1000);
 
 	// Remove the top most comment;
 	if($("#chatHistory div").children().length > 10){
 		$("#chatHistory div").eq(0).remove();
 	}
 });
-
-database.ref("chat").on('child_removed', function(event) {
-	$("#chatHistory").empty();
-	console.log("this event is firing this many times");
-});
-
 
 // =============================
 // ===== Firebase Control ======
@@ -134,7 +132,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 					name: username,
 					wins: 0
 				});
-				updateNameTags();
+				game.updateNameTags();
 			}else if(playerOneExists && !playerTwoExists){
 				player.num = 2;
 				database.ref("players/2").set({
@@ -142,7 +140,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 					name: username,
 					wins: 0
 				});
-				updateNameTags();
+				game.updateNameTags();
 			}
 			$("#inputName").val('');
 		});
@@ -187,10 +185,14 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 			// Check if both player 1 & player 2 exist
 			if(snapshot.child("players/1").exists() && snapshot.child("players/2").exists() && game.turn < 3){
-				readyPlayer(snapshot);
+				game.readyPlayer(snapshot);
 			}else {
-				highlightPlayer(0);
+				game.highlightPlayer(0);
 			}
+
+			// if(snapshot.child("players/1/choice").exists() && game.turn === 2){
+			// 	$("#p1-info").addClass('hide');
+			// }
 
 			// Check who won the game
 			if(snapshot.child("turn").val() === 3 && 
@@ -210,40 +212,39 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 				console.log("Check Winner: compare choices and determine winner");
 				console.log("P1 Choice: " + p1_choice + ", P2 Choice: " + p2_choice);
-				if (p1_choice === "Rock" && p2_choice === "Scissors"){
+				if (p1_choice === "rock" && p2_choice === "scissors"){
 					winner = 1;
 					loser = 2;
-				}else if (p1_choice === "Rock" && p2_choice === "Paper") {
+				}else if (p1_choice === "rock" && p2_choice === "paper") {
 					winner = 2;
 					loser = 1;
-				}else if (p1_choice === "Rock" && p2_choice === "Rock") {
+				}else if (p1_choice === "rock" && p2_choice === "rock") {
 					winner = 0;
 					loser = 0;
-				}else if (p1_choice === "Paper" && p2_choice === "Rock"){
+				}else if (p1_choice === "paper" && p2_choice === "rock"){
 					winner = 1;
 					loser = 2;
-				}else if (p1_choice === "Paper" && p2_choice === "Scissors"){
+				}else if (p1_choice === "paper" && p2_choice === "scissors"){
 					winner = 2;
 					loser = 1;
-				}else if (p1_choice === "Paper" && p2_choice === "Paper"){
+				}else if (p1_choice === "paper" && p2_choice === "paper"){
 					winner = 0;
 					loser = 0;
-				}else if (p1_choice === "Scissors" && p2_choice === "Paper") {
+				}else if (p1_choice === "scissors" && p2_choice === "paper") {
 					winner = 1;
 					loser = 2;
-				}else if (p1_choice === "Scissors" && p2_choice === "Rock") {
+				}else if (p1_choice === "scissors" && p2_choice === "rock") {
 					winner = 2;
 					loser = 1;
-				}else if (p1_choice === "Scissors" && p2_choice === "Scissors") {
+				}else if (p1_choice === "scissors" && p2_choice === "scissors") {
 					winner = 0;
 					loser = 0;
 				}
-				displayWinner(snapshot,winner,loser);
-
+				game.displayWinner(snapshot,winner,loser);
 
 				console.log("Check Winner: reset after timeout");
 				setTimeout(function(){
-					resetGame(snapshot);
+					game.resetGame(snapshot);
 				}, game.roundReset);
 			}
 
@@ -258,9 +259,9 @@ firebase.auth().onAuthStateChanged(function(user) {
 		// End database.ref().on('value')
 		
 		database.ref().on('child_removed', function(snapshot) {
-			console.log('a child has been removed');
-			displayJoinForm("show");
-			displayPlayerTurn("hide");
+			// console.log('a child has been removed');
+			game.displayJoinForm("show");
+			game.displayPlayerTurn("hide");
 		});
 
 		// -------------------------------
@@ -301,121 +302,145 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 // ------------
-// Functions
+// Game Object
 // ------------
+var game = {
+    turn: 0,
+    hasStarted: false,
+    hasSelectedChoice: false,
+    roundReset: 5000,
+    p1_comment_sound: new Audio('assets/audio/button-09.mp3'),
+    p2_comment_sound: new Audio('assets/audio/button-11.mp3'),
 
-// Hide the name input form & display the player's name and assigned number
-function updateNameTags() {
-	const username = $("#inputName").val().trim();
-	displayJoinForm("hide");
-	displayPlayerTurn("show", player.num, username);
-};
+    // Hide the name input form & display the player's name and assigned number
+    updateNameTags: function(){
+    	const username = $("#inputName").val().trim();
+		game.displayJoinForm("hide");
+		game.displayPlayerTurn("show", player.num, username);
+    },
 
-// Hide/Show name input form
-function displayJoinForm(display){
-	if(display === "show"){
-		$("#joinForm").css('display', 'flex');
-	}else if(display === "hide"){
-		$("#joinForm").css('display', 'none');
-	}
-};
+    // Hide/Show name input form
+    displayJoinForm: function(display){
+	    if(display === "show"){
+			$("#joinForm").css('display', 'flex');
+		}else if(display === "hide"){
+			$("#joinForm").css('display', 'none');
+		}	
+    },
 
-// Display the player's name and player number
-function displayPlayerTurn(display, playerNum, playerName){
-	if(display === "show"){
-		$("#playerTurn").css('display', 'block');
-		$("#playerInfo p").text("Hi " + playerName + "! You are Player " + playerNum);
-	}else if(display === "hide"){
-		$("#playerTurn").css('display', 'none');
-	}
-};
+    // Display the player's name and player number
+    displayPlayerTurn: function(display, playerNum, playerName){
+    	if(display === "show"){
+			$("#playerTurn").css('display', 'block');
+			$("#playerInfo p").text("Hi " + playerName + "! You are Player " + playerNum);
+		}else if(display === "hide"){
+			$("#playerTurn").css('display', 'none');
+		}
+    },
 
-// function displayOptions(display, playerNum){
-// 	if(display === "show"){
-// 		$("#p"+ playerNum +"-info").removeClass('hide');
-// 		$(".rock, .paper, .scissors").removeClass('hide');
-// 	}else {
-// 		$("#p"+ activePlayerNum +"-info").addClass('hide');
-// 		$(".rock, .paper, .scissors").addClass('hide');
-// 	}
-// }
+	// * update the 'status' to notify the user if it is their turn or 
+	//   if they are waiting for the other player to move
+    updateTurnStatus: function(display, playerNum, activePlayerNum, activePlayerName) {
+    	if(display === "show"){
+			$("#turnStatus").css('display', 'block');
 
-// * update the 'status' to notify the user if it is their turn or 
-//   if they are waiting for the other player to move
-function updateTurnStatus(display, playerNum, activePlayerNum, activePlayerName){
-	if(display === "show"){
-		$("#turnStatus").css('display', 'block');
+			if(playerNum === activePlayerNum) {
+				$("#turnStatus p").text("It's Your Turn!");
+				game.displayOptions("show", playerNum);
+				// displayOptions("hide", (playerNum === 1) ? 2 : 1);
+			}else{
+				$("#turnStatus p").text("Waiting for " + activePlayerName + " to choose.");
+				$("#p"+ activePlayerNum +"-info").addClass('hide');
+				game.displayOptions("hide", playerNum);
+			}
 
-		if(playerNum === activePlayerNum) {
-			$("#turnStatus p").text("It's Your Turn!");
+		}else if (display === "hide"){
+			$("#turnStatus").css('display', 'none');
+		}
+    },
+
+    // Displays all Options for the
+    displayOptions: function(display, playerNum){
+    	if(display === "show"){
 			$("#p"+ playerNum +"-info").removeClass('hide');
+			$("#p"+ (playerNum === 1) ? 2 : 1 +"-info").addClass('hide'); //TODO: this may be hiding the buttons unexpectently 
 			$(".rock, .paper, .scissors").show();
-		}else{
-			$("#turnStatus p").text("Waiting for " + activePlayerName + " to choose.");
+		}else {
 			$("#p"+ playerNum +"-info").addClass('hide');
-			$("#p"+ activePlayerNum +"-info").addClass('hide');
+			$("#p"+ (playerNum === 1) ? 2 : 1 +"-info").removeClass('hide');
 			$(".rock, .paper, .scissors").hide();
 		}
+    },
 
-	}else if (display === "hide"){
-		$("#turnStatus").css('display', 'none');
-	}
-};
+    // Reveals the chosen responses to both players (after a winner has been chosen)
+    reveal: function(display){
+    	if(display === "show"){
+			$("#p1-info, #p2-info").removeClass('hide');
+		}else {
+			$("#p1-info, #p2-info").addClass('hide');
+		}
+    },
 
-// Highlight the selected player
-function highlightPlayer(playerNum){
-	$(".player").removeClass('bg-warning')
-	$("#player-" + playerNum).addClass('bg-warning');
-};
+    // Highlight the selected player
+    highlightPlayer: function(playerNum){
+    	$(".player").removeClass('bg-warning')
+		$("#player-" + playerNum).addClass('bg-warning');
+    },
 
-// Update database to reflect player's choice for this round
-function updatePlayerChoice(selectedOption){
-	(game.turn === 1) ? game.turn = 2 : game.turn = 3;
+	// Update database to reflect player's choice for this round
+    updatePlayerChoice: function(selectedOption){
+    	(game.turn === 1) ? game.turn = 2 : game.turn = 3;
 
-	game.hasSelectedChoice = true;
+		game.hasSelectedChoice = true;
 
-	database.ref().update({
-		turn: game.turn
-	});
-	database.ref("players/" + player.num).update({
-		choice: selectedOption
-	});
-};	
-
-// Increment wins/losses and update the DOM with the winner
-function displayWinner(snap, winner,loser){
-	if(!(winner === 0) && !(loser === 0)){
-		console.log('Check Winner: Player '+ winner +' wins!');
-		let winner_wins = snap.child("players/" + winner).val().wins;
-		let loser_losses = snap.child("players/" + loser).val().losses;
-		winner_wins++;
-		loser_losses++;
-		database.ref("players/" + winner).update({
-			wins: winner_wins
+		database.ref().update({
+			turn: game.turn
 		});
-		database.ref("players/" + loser).update({
-			losses: loser_losses
+		database.ref("players/" + player.num).update({
+			choice: selectedOption
 		});
+    },
 
-		$("#results").text(snap.child("players/" + winner).val().name + " Wins!!");		
-	}else{
-		console.log('Check Winner: Tie Game');
-		$("#results").text("Tie Game!");	
-	}
-};
+    // Increment wins/losses and update the DOM with the winner
+    displayWinner: function(snap, winner,loser){
+    	if(!(winner === 0) && !(loser === 0)){
+			console.log('Check Winner: Player '+ winner +' wins!');
+			let winner_wins = snap.child("players/" + winner).val().wins;
+			let loser_losses = snap.child("players/" + loser).val().losses;
+			winner_wins++;
+			loser_losses++;
+			database.ref("players/" + winner).update({
+				wins: winner_wins
+			});
+			database.ref("players/" + loser).update({
+				losses: loser_losses
+			});
 
-function resetGame(snap){
-	game.hasSelectedChoice = false;
-	$("#results").text('');
-	// $(".rock, .paper, .scissors").show();
-	readyPlayer(snap);
+			$("#results").text(snap.child("players/" + winner).val().name + " Wins!!");		
+		}else{
+			console.log('Check Winner: Tie Game');
+			$("#results").text("Tie Game!");	
+		}
+		game.reveal("show");
+    },
 
-	console.log('game has been reset');
-}
 
-function readyPlayer(snap){
-	updateTurnStatus("show",player.num,game.turn,snap.child("players/" + game.turn).val().name);
-	highlightPlayer(game.turn);
+    resetGame: function(snap){
+    	game.hasSelectedChoice = false;
+		$("#results").text('');
+		// $(".rock, .paper, .scissors").show();
+		game.readyPlayer(snap);
+		game.displayOptions("show", player.num);
+		game.displayOptions("hide", (player.num === 1) ? 2 : 1);
+
+		console.log('game has been reset');
+    },
+
+
+    readyPlayer: function(snap){
+    	game.updateTurnStatus("show",player.num,game.turn,snap.child("players/" + game.turn).val().name);
+		game.highlightPlayer(game.turn);
+    }
 }
 
 // ============================
@@ -425,31 +450,23 @@ function readyPlayer(snap){
 // ------------
 // Event Handlers
 // ------------
-$(".card-body").on('click', ".rock", function(event) {
+
+$(".player .btn").on('click', function(event) {
 	event.preventDefault();
 	if(!game.hasSelectedChoice){
-		updatePlayerChoice("Rock");
-		$(".paper, .scissors").hide();
-	}
-}).on('click', '.paper', function(event) {
-	event.preventDefault();
-	if(!game.hasSelectedChoice){
-		updatePlayerChoice("Paper");
-		$(".rock, .scissors").hide();	
-	}
-}).on('click', '.scissors', function(event) {
-	event.preventDefault();
-	if(!game.hasSelectedChoice){
-		updatePlayerChoice("Scissors");
-		$(".rock, .paper").hide();	
+		var choice = $(this).attr('data-value');
+		game.updatePlayerChoice(choice);
+		$(".player .btn:not(."+ choice +")").hide();
 	}
 });
 
 $(".reset").on('click', function(event) {
 	event.preventDefault();
 	database.ref().remove();
-	displayJoinForm("show");
-	displayPlayerTurn("hide");
+	$("#chatHistory").empty();
+	game.displayJoinForm("show");
+	game.displayPlayerTurn("hide");
+	// $("#p2-info").addClass('hide');
 });
 
 // ------------
@@ -457,12 +474,3 @@ $(".reset").on('click', function(event) {
 // -----------
 
 // * Hide options once choice has been made & display chosen option / change state to prevent further clicking
-
-// Global Variables needed:
-var game = {
-    turn: 0,
-    hasSelectedChoice: false,
-    roundReset: 5000
-}
-
-
